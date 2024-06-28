50a463b72d2664909a3f1019dc3d7d03ba991408
Jake Soenneker <jake@soenneker.com>
2024-06-28 16:08:16 -0500

    Better initialization

--- src/ApplicationInsightsInterop.cs
+++ src/ApplicationInsightsInterop.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.JSInterop;
 using Soenneker.Blazor.ApplicationInsights.Abstract;
 using Soenneker.Blazor.Utils.ModuleImport.Abstract;
+using Soenneker.Extensions.ValueTask;
+using Soenneker.Utils.AsyncSingleton;
 
 namespace Soenneker.Blazor.ApplicationInsights;
 
@@ -14,37 +16,36 @@ public class ApplicationInsightsInterop : IApplicationInsightsInterop
     private readonly ILogger<ApplicationInsightsInterop> _logger;
     private readonly IModuleImportUtil _moduleImportUtil;
 
-    private bool _initialized;
+    private readonly AsyncSingleton<object> _scriptInitializer;
 
     public ApplicationInsightsInterop(IJSRuntime jSRuntime, ILogger<ApplicationInsightsInterop> logger, IModuleImportUtil moduleImportUtil)
     {
         _jsRuntime = jSRuntime;
         _logger = logger;
         _moduleImportUtil = moduleImportUtil;
-    }
 
-    private async ValueTask EnsureInitialization(CancellationToken cancellationToken = default)
-    {
-        if (_initialized)
-            return;
+        _scriptInitializer = new AsyncSingleton<object>(async objects =>
+        {
+            var cancellationToken = (CancellationToken) objects[0];
 
-        _initialized = true;
+            await _moduleImportUtil.Import("Soenneker.Blazor.ApplicationInsights/applicationinsightsinterop.js", cancellationToken);
+            await _moduleImportUtil.WaitUntilLoadedAndAvailable("Soenneker.Blazor.ApplicationInsights/applicationinsightsinterop.js", "AppInsightsInterop", 100, cancellationToken);
 
-        await _moduleImportUtil.Import("Soenneker.Blazor.ApplicationInsights/js/applicationinsightsinterop.js", cancellationToken);
-        await _moduleImportUtil.WaitUntilLoaded("Soenneker.Blazor.ApplicationInsights/js/applicationinsightsinterop.js", cancellationToken);
+            return new object();
+        });
     }
 
     public async ValueTask Init(string connectionString, CancellationToken cancellationToken = default)
     {
         _logger.LogDebug("Initializing Application Insights...");
 
-        await EnsureInitialization(cancellationToken);
+        await _scriptInitializer.Get(cancellationToken).NoSync();
 
         await _jsRuntime.InvokeVoidAsync("AppInsightsInterop.init", cancellationToken, connectionString);
     }
 
     public ValueTask DisposeAsync()
     {
-        return _moduleImportUtil.DisposeModule("Soenneker.Blazor.ApplicationInsights/js/applicationinsightsinterop.js");
+        return _moduleImportUtil.DisposeModule("Soenneker.Blazor.ApplicationInsights/applicationinsightsinterop.js");
     }
 }
