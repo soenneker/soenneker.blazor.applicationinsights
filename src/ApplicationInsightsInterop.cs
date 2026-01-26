2adfe5f4192c28c2e30f8688122ffbc640e536ba
Jake Soenneker <jake@soenneker.com>
2026-01-26 16:27:50 -0600

    Adds cancellationscope

--- src/ApplicationInsightsInterop.cs
+++ src/ApplicationInsightsInterop.cs
@@ -2,9 +2,11 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.JSInterop;
+using Soenneker.Asyncs.Initializers;
 using Soenneker.Blazor.ApplicationInsights.Abstract;
 using Soenneker.Blazor.Utils.ResourceLoader.Abstract;
-using Soenneker.Asyncs.Initializers;
+using Soenneker.Extensions.CancellationTokens;
+using Soenneker.Utils.CancellationScopes;
 
 namespace Soenneker.Blazor.ApplicationInsights;
 
@@ -19,31 +21,46 @@ public sealed class ApplicationInsightsInterop : IApplicationInsightsInterop
 
     private const string _module = "Soenneker.Blazor.ApplicationInsights/applicationinsightsinterop.js";
 
-    public ApplicationInsightsInterop(IJSRuntime jSRuntime, ILogger<ApplicationInsightsInterop> logger, IResourceLoader resourceLoader)
+    private readonly CancellationScope _cancellationScope = new();
+
+    public ApplicationInsightsInterop(
+        IJSRuntime jSRuntime,
+        ILogger<ApplicationInsightsInterop> logger,
+        IResourceLoader resourceLoader)
     {
         _jsRuntime = jSRuntime;
         _logger = logger;
         _resourceLoader = resourceLoader;
+
         _scriptInitializer = new AsyncInitializer(InitializeScript);
     }
 
     private ValueTask InitializeScript(CancellationToken token)
     {
-        return _resourceLoader.ImportModuleAndWaitUntilAvailable(_module, "AppInsightsInterop", 100, token);
+        return _resourceLoader.ImportModuleAndWaitUntilAvailable(
+            _module,
+            "AppInsightsInterop",
+            100,
+            token);
     }
 
     public async ValueTask Init(string connectionString, CancellationToken cancellationToken = default)
     {
         _logger.LogDebug("Initializing Application Insights...");
 
-        await _scriptInitializer.Init(cancellationToken);
+        var linked = _cancellationScope.CancellationToken.Link(cancellationToken, out var source);
 
-        await _jsRuntime.InvokeVoidAsync("AppInsightsInterop.init", cancellationToken, connectionString);
+        using (source)
+        {
+            await _scriptInitializer.Init(linked);
+            await _jsRuntime.InvokeVoidAsync("AppInsightsInterop.init", linked, connectionString);
+        }
     }
 
     public async ValueTask DisposeAsync()
     {
         await _resourceLoader.DisposeModule(_module);
         await _scriptInitializer.DisposeAsync();
+        await _cancellationScope.DisposeAsync();
     }
 }
